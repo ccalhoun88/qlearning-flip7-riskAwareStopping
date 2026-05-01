7d7ab3c8e0698326ebefd38daf454da3644d7d60
ccalhoun88 <diante.calhoun@gmail.com>
2026-05-01 14:18:13 -0500

    Implement logging for engine and training
    Added logging setup for engine events and training metrics.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# Added logging for the two loggers in the engine and the training
+import logging
 
+def setup_logging():
+    # Engine game events
+    engine_handler = logging.FileHandler("flip7_game_log.txt", mode="w")
+    engine_handler.setFormatter(logging.Formatter("%(message)s"))
+    logging.getLogger("flip7.engine").addHandler(engine_handler)
+    logging.getLogger("flip7.engine").setLevel(logging.INFO)
+
+    # Training metrics
+    training_handler = logging.FileHandler("flip7_training_log.txt", mode="w")
+    training_handler.setFormatter(logging.Formatter("%(message)s"))
+    logging.getLogger("flip7.training").addHandler(training_handler)
+    logging.getLogger("flip7.training").setLevel(logging.INFO)
